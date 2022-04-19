8e096171e4f07d0b4d76c095881c4d25d158cc05
puhbu <78015856+poob1@users.noreply.github.com>
2022-04-19 21:39:14 +0300

    clown plugin soon
    yes

--- Moyai/src/main/kotlin/me/aniimalz/plugins/Moyai.kt
+++ Moyai/src/main/kotlin/me/aniimalz/plugins/Moyai.kt
@@ -1,4 +1,4 @@
-package me.aniimalz.plugins
+package me.puhbu.plugins
 
 import android.content.Context
 import android.media.AudioAttributes
@@ -41,7 +41,7 @@ class Moyai : Plugin() {
             val message = Message(this)
             val content = message.content.lowercase()
             if (message.channelId != StoreStream.getChannelsSelected().id) return@subscribe
-            if (content.contains("🗿") || content.contains("vine boom")) funny()
+            if (content.contains("🤡") || content.contains("clown")) funny()
         }
 
     }
