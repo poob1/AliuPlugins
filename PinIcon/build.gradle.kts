b6c42bf29e6bcfed21eed5f5ff0926103ed56f69
puhbu <78015856+poob1@users.noreply.github.com>
2022-04-19 21:41:24 +0300

    Delete PinIcon directory

--- PinIcon/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-version = "1.1.0"
-description = "Shows a pin icon next to messages that have been pinned"
-
-aliucord {
-    author("Tyman", 487443883127472129L) // helped with weird crashing and stuff
-}
