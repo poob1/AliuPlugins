84a0097a642f179f8fae687af1f012aecef4d5c0
puhbu <78015856+poob1@users.noreply.github.com>
2022-04-19 21:37:53 +0300

    Delete MoarConfirm directory

--- MoarConfirm/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-version = "1.0.6"
-description = "More confirmations on shit."
-
-aliucord {
-    changelog.set("""
-        ## 1.0.4
-        - Added confirmation for deleting a role
-    """.trimIndent())
-}
